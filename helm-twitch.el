495ee64657e01f9b3085d6c33e49afeabfe31c12
Aaron Jacobs <atheriel@gmail.com>
2015-08-27 19:30:36 -0400

    Corrects the header and adds `dash' as a dependency.
    Signed-off-by: Aaron Jacobs <atheriel@gmail.com>

--- helm-twitch.el
+++ helm-twitch.el
@@ -1,8 +1,7 @@
-;; This buffer is for notes you don't want to save, and for Lisp evaluation.
-;; If you want to create a file, visit that file with C-x C-f,
-;; then enter the text in that file's own buffer.
+;;; helm-twitch --- Navigate twitch.tv via Helm
 
 (require 'url)
+(require 'dash)
 (require 'json)
 (require 'helm)
 
