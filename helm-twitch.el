eb05fa711e8064a7719215de6fcc1883ea7b7a6b
Aaron Jacobs <atheriel@gmail.com>
2015-08-27 20:03:49 -0400

    Refactors the internal API and adds a channel & website source.
    Signed-off-by: Aaron Jacobs <atheriel@gmail.com>

--- helm-twitch.el
+++ helm-twitch.el
@@ -46,21 +46,14 @@
   :version 0.1
   :type 'string)
 
-(defun twitch-search (search-term)
-  "Search Twitch for SEARCH-TERM, returning the results as a Lisp structure."
-  (let ((a-url (if (not twitch-game-type)
-		   (format "https://api.twitch.tv/kraken/streams?q=%s&limit=10" search-term)
-		 (format "https://api.twitch.tv/kraken/streams?game=%s&q=%s&limit=10"
-			 twitch-game-type search-term)))
-	(url-request-extra-headers
-	 (and twitch-api-client-id
-	      '(("Client-ID" . twitch-api-client-id))))
-	)
-    ;; (warn a-url)
-    (with-current-buffer
-	(url-retrieve-synchronously a-url)
-      (goto-char url-http-end-of-headers)
-      (json-read))))
+(defun twitch--plist-to-url-params (plist)
+  "Turn a property list into an HTML parameter string."
+  (mapconcat (lambda (entry)
+	       (concat (url-hexify-string
+			(nth 1 (split-string (format "%s" (nth 0 entry)) ":")))
+		       "="
+		       (url-hexify-string (format "%s" (nth 1 entry)))))
+	     (-partition 2 plist) "&"))
 
 (defun twitch-format-stream (stream)
   "Given a STREAM, return a a formatted string suitable for display."
@@ -82,62 +75,6 @@
 	    "  "
 	    (propertize status 'face 'helm-twitch-status-face))))
 
-(defun twitch-search-formatted (search-term)
-  (mapcar (lambda (stream)
-	    (cons (twitch-format-stream stream) stream))
-	  (alist-get '(streams) (twitch-search search-term))))
-
-;; (twitch-search-formatted "fro")
-
-(defun helm-twitch-search ()
-  (twitch-search-formatted helm-pattern))
-
-(defvar helm-source-twitch
-  '((name . "Live Streams")
-    (volatile)
-    ;; (delayed)
-    ;; (requires-pattern . 2)
-    (candidates-process . helm-twitch-search)
-    (action . (("Open this stream"
-		. (lambda (stream) (browse-url (alist-get '(name) stream))))
-	       )))
-  "A `helm' source for Twitch streams.")
-
-(defun helm-twitch-website-search ()
-  "Returns a formatted `helm' candidate for searching twitch.tv directly."
-  (list (cons (concat (propertize "[?]" 'face 'helm-twitch-prefix-face)
-		      (format " search for `%s' in a browser" helm-pattern))
-	helm-pattern)))
-
-(defvar helm-source-twitch-website
-  '((name . "Search twitch.tv directly")
-    (volatile)
-    ;; Require two letters (the smallest number there may be no results for),
-    ;; so that it does not need to show up in the initial buffer.
-    (requires-pattern . 2)
-    (candidates-process . helm-twitch-website-search)
-    (action . (("Open the twitch.tv website with this search term"
-		. (lambda (query)
-		    (browse-url (concat "http://www.twitch.tv/search?query="
-					query))))
-	       )))
-  "A `helm' source for searching Twitch's website directly.")
-
-(defun twitch--search (search-term kind &optional limit)
-  "Search Twitch for SEARCH-TERM, returning the results as a Lisp structure."
-  (let* ((lim (format "&limit=%s" (or limit 10)))
-	 ;; The `game' parameter seems to have no effect in channel searches.
-	 (game (and twitch-game-type
-		    (format "&game=%s" (url-encode-url twitch-game-type))))
-	 (api-url (concat "https://api.twitch.tv/kraken/search/" kind
-			  "?q=" search-term
-			  lim game)))
-    (kill-new api-url)
-    (with-current-buffer
-	(url-retrieve-synchronously api-url)
-      (goto-char url-http-end-of-headers)
-      (json-read))))
-
 (defun twitch-format-channel (channel)
   "Given a CHANNEL, return a a formatted string suitable for display."
   (let* ((followers (format "%6s" (alist-get '(followers) channel)))
@@ -150,10 +87,63 @@
 	    "  "
 	    (propertize game 'face 'helm-twitch-status-face))))
 
+(defun twitch-search-streams (search-term)
+  "Retrieve a list of Twitch streams that match the SEARCH-TERM."
+  (let ((results (if twitch-game-type
+		     (twitch-api "streams" :query search-term :limit 10
+				 :game twitch-game-type)
+		   (twitch-api "streams" :query search-term :limit 10))))
+    (alist-get '(streams) results)))
+
 (defun twitch-search-channels (search-term)
-  (mapcar (lambda (channel)
-	    (cons (twitch-format-channel channel) channel))
-	  (alist-get '(channels) (twitch--search search-term "channels"))))
+  "Retrieve a list of Twitch channels that match the SEARCH-TERM."
+  (alist-get '(channels)
+	     (twitch-api "search/channels" :query search-term :limit 10)))
+
+(defun helm-twitch-website-search (search-term)
+  "Returns a formatted `helm' candidate for searching twitch.tv directly."
+  (list (cons (concat (propertize "[?]" 'face 'helm-twitch-prefix-face)
+		      (format " search for `%s' in a browser" search-term))
+	search-term)))
+
+(defun twitch-api (endpoint &rest plist)
+  "Query the Twitch API at ENDPOINT, returning the results as a Lisp structure.
+
+API parameters can be passed as a property list. For example:
+
+    (twitch-api \"search/channels\" :query \"flame\" :limit 15)
+
+This function does not perform error checking."
+  (let* (;; TODO: Investigate using `url-request-data' instead.
+	 (params (twitch--plist-to-url-params plist))
+	 (api-url (concat "https://api.twitch.tv/kraken/" endpoint "?" params))
+	 ;; Use version 3 of the API.
+	 (url-request-extra-headers
+	  '(("Accept" . "application/vnd.twitchtv.v3+json")))
+	 )
+    ;; Add the client ID as a header (if the user has specified one).
+    (and twitch-api-client-id
+	 (add-to-list url-request-extra-headers
+		      '("Client-ID" . twitch-api-client-id) t))
+    (kill-new api-url) 			; For debugging.
+    (with-current-buffer
+	(url-retrieve-synchronously api-url)
+      (goto-char url-http-end-of-headers)
+      (json-read))))
+
+(defvar helm-source-twitch
+  '((name . "Live Streams")
+    (volatile)
+    (candidates-process
+     . (lambda ()
+	 ;; Format the list of returned streams.
+	 (mapcar (lambda (stream) (cons (twitch-format-stream stream) stream))
+		 (twitch-search-streams helm-pattern))))
+    (action . (("Open this stream"
+		. (lambda (stream)
+		    (browse-url (alist-get '(url) (alist-get '(channel) stream)))))
+	       )))
+  "A `helm' source for Twitch streams.")
 
 (defvar helm-source-twitch-channels
   '((name . "Channels")
@@ -161,11 +151,29 @@
     ;; The twitch.tv API seems to require at least three characters for channel
     ;; searches.
     (requires-pattern . 3)
-    (candidates-process . (lambda () (twitch-search-channels helm-pattern)))
+    (candidates-process
+     . (lambda ()
+	 ;; Format the list of returned channels.
+	 (mapcar (lambda (channel) (cons (twitch-format-channel channel) channel))
+		 (twitch-search-channels helm-pattern))))
     (action . (("Open this channel"
 		. (lambda (stream) (browse-url (alist-get '(url) stream))))
 	       )))
   "A `helm' source for Twitch channels.")
+
+(defvar helm-source-twitch-website
+  '((name . "Search twitch.tv directly")
+    (volatile)
+    ;; Require two letters (the smallest number there may be no results for),
+    ;; so that it does not need to show up in the initial buffer.
+    (requires-pattern . 2)
+    (candidates-process . (lambda () (helm-twitch-website-search helm-pattern)))
+    (action . (("Open the twitch.tv website with this search term"
+		. (lambda (query)
+		    (browse-url (concat "http://www.twitch.tv/search?query="
+					query))))
+	       )))
+  "A `helm' source for searching Twitch's website directly.")
 
 (defun helm-twitch ()
   "Search for live Twitch streams with `helm'."
